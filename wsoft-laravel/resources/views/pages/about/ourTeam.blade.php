decf98c47ea05f85c5aaff248c36f9dd9e94998a
Shalaka Gajanayake <shalakagajanayake123@gmail.com>
2025-08-03 13:03:34 +0530

    ourTeam.blade.php - updated

--- wsoft-laravel/resources/views/pages/about/ourTeam.blade.php
+++ wsoft-laravel/resources/views/pages/about/ourTeam.blade.php
@@ -1,43 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<!-- Mirrored from mannatstudio.com/html/serenite/our-services.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 30 Jul 2025 13:57:36 GMT -->
+@section('title', 'Our Team - Wsoft Fullstack Software Solutions & Development Services')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title>Wsoft &#8211; Fullstack Software Solutions & Development Services</title>
-
-    <meta name="author" content="Mannat Studio">
-    <meta name="description"
-        content="Serenite is a Responsive HTML5 Template for SaaS, cryptocurrency, app and tech companies, as well as for digital studios.">
-    <meta name="keywords"
-        content="Serenite, themeforest template, app, app landing page, App Showcase, cryptocurrency, digital studio, saas, saas landing, saas theme, software, software company website, software startup, startup, startup landing page, startup wordpress, technology">
-
-    <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com/">
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500&amp;display=swap"
-        rel="stylesheet">
-    <!-- Plugin Style CSSS -->
-    <link rel="stylesheet" href="{{ asset('css/theme-plugins.min.css') }}">
-    <!-- Main Template CSS -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</head>
-
-<body>
-
-    <!-- Page loader Start -->
-    <div id="pageloader">
-        <div class="loader-item">
-            <img src="{{ asset('images/tail-spin.svg') }}" width="80" alt="">
-        </div>
-    </div>
-    <!-- Page loader End -->
+@section('content')
 
     <!-- Page Header Start -->
     <div class="bg-primary header-bg">
@@ -72,9 +37,9 @@
                             <i class="srn-search"></i>
                         </a>
                         <!-- <a class="signup-btn btn btn-outline-light text-nowrap" data-trigger="#signup">
-                        <span class="d-none d-sm-inline-block"><span class="outer-wrap"><span data-text="Sing Up">Sing Up</span></span></span>
-                        <span class="d-block d-sm-none"><i class="bi bi-door-closed"></i></span>
-                    </a> -->
+                            <span class="d-none d-sm-inline-block"><span class="outer-wrap"><span data-text="Sing Up">Sing Up</span></span></span>
+                            <span class="d-block d-sm-none"><i class="bi bi-door-closed"></i></span>
+                        </a> -->
                         <button class="navbar-toggler x collapsed" type="button" data-bs-toggle="offcanvas"
                             data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
                             aria-label="Toggle navigation">
@@ -146,8 +111,8 @@
                                     Policy</a>.</small>
                         </div>
                         <div class="d-grid">
-                            <button type="button" class="btn btn-outline-primary btn-sm"><span
-                                    class="outer-wrap"><span data-text="Singup">Singup</span></span></button>
+                            <button type="button" class="btn btn-outline-primary btn-sm"><span class="outer-wrap"><span
+                                        data-text="Singup">Singup</span></span></button>
                         </div>
                     </div>
                 </div>
@@ -371,8 +336,7 @@
                     <i class="srn-search"></i>
 
                     <div class="w-100">
-                        <input class="form-control form-light" type="text" placeholder="Search"
-                            aria-label="Search">
+                        <input class="form-control form-light" type="text" placeholder="Search" aria-label="Search">
                     </div>
                     <a href="#" class="overlay-close link-oragne"><i class="bi bi-x"></i></a>
                 </div>
@@ -381,21 +345,4 @@
     </div>
     <!-- Search Popup End -->
 
-
-    <!-- Jquery Library JS -->
-    <script data-cfasync="false" src="{{ asset('cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js') }}">
-    </script>
-    <script src="{{ asset('js/jquery-min.js') }}"></script>
-    <!-- Theme Plugin -->
-    <script src="{{ asset('js/theme-plugins.min.js') }}"></script>
-    <!-- Theme Custom -->
-    <script src="{{ asset('js/script.js') }}"></script>
-    <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015"
-        integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ=="
-        data-cf-beacon='{"version":"2024.11.0","token":"64224fc8786846928480d180dfc466bd","r":1,"server_timing":{"name":{"cfCacheStatus":true,"cfEdge":true,"cfExtPri":true,"cfL4":true,"cfOrigin":true,"cfSpeedBrain":true},"location_startswith":null}}'
-        crossorigin="anonymous"></script>
-</body>
-
-<!-- Mirrored from mannatstudio.com/html/serenite/our-services.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 30 Jul 2025 13:57:36 GMT -->
-
-</html>
+@endsection
