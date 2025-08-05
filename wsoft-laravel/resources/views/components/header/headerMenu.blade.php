39bd771a25335cd49998f98e1f95cc96d50df435
Shalaka Gajanayake <shalakagajanayake123@gmail.com>
2025-08-05 08:09:33 +0530

    headerMenu.blade.php - header menu mobile view updated

--- wsoft-laravel/resources/views/components/header/headerMenu.blade.php
+++ wsoft-laravel/resources/views/components/header/headerMenu.blade.php
@@ -1,41 +1,24 @@
  <div class="offcanvas-body" data-lenis-prevent>
 
      <ul class="navbar-nav">
-         <li class="nav-item dropdown">
-             <a class="nav-link" href="{{ url('/') }}" aria-expanded="false">Home</a>
-             <!-- <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="{{ url('/') }}">Home Default</a></li>
-                                    <li><a class="dropdown-item" href="index-digital-marketing.html">Home Digital Marketing</a></li>
-                                    <li><a class="dropdown-item" href="index-software.html">Home Software</a></li>
-                                    <li><a class="dropdown-item" href="index-coworking.html">Home Coworking</a></li>
-                                </ul> -->
+         <li class="nav-item">
+             <a class="nav-link" href="{{ url('/') }}" >Home</a>
          </li>
-         <li class="nav-item dropdown">
-             <a class="nav-link" href="{{ url('/services') }}" aria-expanded="false">Services</a>
+         <li class="nav-item ">
+             <a class="nav-link " href="{{ url('/services') }}">Services</a>
          </li>
 
-         <li class="nav-item dropdown">
-             <a class="nav-link" href="{{ url('/portfolio') }}" aria-expanded="false">Portfolio</a>
-             <!-- <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="{{ url('/portfolio') }}">Portfolio 2 Column</a></li>
-                                    <li><a class="dropdown-item" href="portfolio-3col.html">Portfolio 3 Column</a></li>
-                                    <li><a class="dropdown-item" href="portfolio-4col.html">Portfolio 4 Column</a></li>
-                                    <li><a class="dropdown-item" href="portfolio-single.html">Portfolio Single</a></li>
-                                </ul> -->
+         <li class="nav-item ">
+             <a class="nav-link " href="{{ url('/portfolio') }}">Portfolio</a>
          </li>
-         <li class="nav-item dropdown">
-             <a class="nav-link " href="{{ url('/about') }}" aria-expanded="false">About</a>
-             {{-- <ul class="dropdown-menu">
-                 <li><a class="dropdown-item" href="{{ url('/about') }}">About Us</a></li>
-                 <li><a class="dropdown-item" href="{{ url('/about/team') }}">Our Team</a></li>
-                 <!-- <li><a class="dropdown-item" href="team-single.html">Team Single</a></li> -->
-             </ul> --}}
+         <li class="nav-item ">
+             <a class="nav-link " href="{{ url('/about') }}" >About</a>
          </li>
-         <li class="nav-item dropdown">
-             <a class="nav-link" href="{{ url('/faq') }}" aria-expanded="false">FAQs</a>
+         <li class="nav-item ">
+             <a class="nav-link " href="{{ url('/faq') }}" >FAQs</a>
          </li>
-         <li class="nav-item dropdown">
-             <a class="nav-link" href="{{ url('/contact') }}" aria-expanded="false">Contact
+         <li class="nav-item ">
+             <a class="nav-link " href="{{ url('/contact') }}" >Contact
                  Us</a>
          </li>
          <li class="nav-item dropdown">
@@ -50,27 +33,6 @@
                  <!-- <li><a class="dropdown-item" href="service-single.html">Service Details</a></li> -->
              </ul>
          </li>
-         <!-- <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Blog</a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="blog-grid.html">Blog Grid</a></li>
-                                    <li><a class="dropdown-item" href="blog-standard.html">Blog Standard</a></li>
-                                    <li><a class="dropdown-item" href="blog-single.html">Blog Single</a></li>
-                                </ul>
-                            </li> -->
-         <!-- <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Elements</a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="typography.html#heading">Headings Styles</a></li>
-                                    <li><a class="dropdown-item" href="typography.html#icons">Icons Styles</a></li>
-                                    <li><a class="dropdown-item" href="typography.html#button-style">Button Styles</a></li>
-                                    <li><a class="dropdown-item" href="typography.html#blockquotes-styles">Blockquotes Styles</a></li>
-                                    <li><a class="dropdown-item" href="typography.html#bullet-styles">Bullet Styles</a></li>
-                                    <li><a class="dropdown-item" href="typography.html#tabs-styles">Tabs Styles</a></li>
-                                    <li><a class="dropdown-item" href="typography.html#accordian-styles">Accordian Styles</a></li>
-                                    <li><a class="dropdown-item" href="typography.html#forms-styles">Forms Styles</a></li>
-                                    <li><a class="dropdown-item" href="typography.html#pagination-styles">Pagination Styles</a></li>
-                                </ul>
-                            </li> -->
+
      </ul>
  </div>
